8f85a955758c6d3a69848c766b35411b0fe79ea5
yourbreathtaking <52409746+yourbreathtaking@users.noreply.github.com>
2024-12-14 15:26:58 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="viewer">
     <header id="header">
       <div class="row">
-        <h1 class="title">visualization geoparquet</h1>
+        <h1 class="title">Visualisation geoparquet</h1>
 
       </div>
       <div class="row subheader">
